9ce787aa8d9ad7f3ee8065a4311497dd999eeaa6
Subram <subram.iitb@gmail.com>
2023-03-24 07:39:03 +0530

    All Home done

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import { CiWarning,CiMenuFries,CiUser,CiShop } from "react-icons/ci";
 import { HiOutlineShoppingBag } from "react-icons/hi";
+
+
 import Home0 from "./components/Home1/Home0";
 import Home1 from "./components/Home1/Home1";
 import Home2 from "./components/Home1/Home2";
 import Home3 from "./components/Home1/Home3";
 import Home4 from "./components/Home1/Home4";
 import Home5 from "./components/Home1/Home5";
-// import './App.css';
 
 function App() {
   return (
@@ -57,11 +58,11 @@ function App() {
       </body> */}
 
 
-    </>
 
 
    
 
+    </>
   );
 }
 
